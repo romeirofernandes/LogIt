d6e36971a8c20cf15ab76db8dbe0706095a3d613
Romeiro <theromeirofernandes@gmail.com>
2025-05-23 23:44:02 +0530

    initialized react project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center tracking- min-h-screen max-w-4xl mx-auto bg-neutral-100">
+        <h1 className="text-4xl font-bold">Hello World</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
